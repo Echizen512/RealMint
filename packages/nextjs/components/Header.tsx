a284ccebf3bd60ddb11b3caeea5320c7344fe2f6
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-08-30 08:10:08 -0400

    feat: update Header component structure and styling; add new logo image

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,8 +4,8 @@ import React, { useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Bug, Home, Info, Menu, Upload, User } from "lucide-react";
 import { hardhat } from "viem/chains";
-import { Home, Upload, User, Bug, Menu, Info } from "lucide-react"
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick, useTargetNetwork } from "~~/hooks/scaffold-eth";
 
@@ -55,8 +55,9 @@ export const HeaderMenuLinks = () => {
             <Link
               href={href}
               passHref
-              className={`${isActive ? "bg-secondary shadow-md" : ""
-                } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
+              className={`${
+                isActive ? "bg-secondary shadow-md" : ""
+              } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
             >
               {icon}
               <span>{label}</span>
@@ -79,39 +80,39 @@ export const Header = () => {
   useOutsideClick(burgerMenuRef, () => {
     burgerMenuRef?.current?.removeAttribute("open");
   });
-return (
-  <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
-    <div className="navbar-start w-auto lg:w-1/2">
-      <details className="dropdown" ref={burgerMenuRef}>
-        <summary className="ml-1 btn btn-ghost lg:hidden hover:bg-transparent">
-          <Menu className="h-5 w-5" /> 
-        </summary>
-        <ul
-          className="menu menu-compact dropdown-content mt-3 p-2 shadow-sm bg-base-100 rounded-box w-52"
-          onClick={() => {
-            burgerMenuRef?.current?.removeAttribute("open");
-          }}
-        >
+  return (
+    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
+      <div className="navbar-start w-auto lg:w-1/2">
+        <details className="dropdown" ref={burgerMenuRef}>
+          <summary className="ml-1 btn btn-ghost lg:hidden hover:bg-transparent">
+            <Menu className="h-5 w-5" />
+          </summary>
+          <ul
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow-sm bg-base-100 rounded-box w-52"
+            onClick={() => {
+              burgerMenuRef?.current?.removeAttribute("open");
+            }}
+          >
+            <HeaderMenuLinks />
+          </ul>
+        </details>
+        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
+          <div className="flex relative w-10 h-10">
+            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.png" />
+          </div>
+          <div className="flex flex-col">
+            <span className="font-bold leading-tight">RWS-Forge</span>
+            {/* <span className="text-xs">Ethereum dev stack</span> */}
+          </div>
+        </Link>
+        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
           <HeaderMenuLinks />
         </ul>
-      </details>
-      <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
-        <div className="flex relative w-10 h-10">
-          <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
-        </div>
-        <div className="flex flex-col">
-          <span className="font-bold leading-tight">Scaffold-ETH</span>
-          <span className="text-xs">Ethereum dev stack</span>
-        </div>
-      </Link>
-      <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
-        <HeaderMenuLinks />
-      </ul>
+      </div>
+      <div className="navbar-end grow mr-4">
+        <RainbowKitCustomConnectButton />
+        {isLocalNetwork && <FaucetButton />}
+      </div>
     </div>
-    <div className="navbar-end grow mr-4">
-      <RainbowKitCustomConnectButton />
-      {isLocalNetwork && <FaucetButton />}
-    </div>
-  </div>
-);
+  );
 };
